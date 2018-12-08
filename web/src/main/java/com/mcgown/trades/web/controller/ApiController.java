2e5a3f8cf485aa80c8ea131eb2307c8b74adcec0
Brian McGowan <bmcgowan@overstock.com>
2018-12-08 19:56:54 +0000

    cross import module working

--- web/src/main/java/com/mcgown/trades/web/controller/ApiController.java
+++ web/src/main/java/com/mcgown/trades/web/controller/ApiController.java
@@ -4,14 +4,17 @@ import lombok.extern.log4j.Log4j2;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mcgowan.trades.common.TradeDTO;
 
 @RestController
 @Log4j2
 public class ApiController {
 
   @PostMapping("/trade")
-  public HttpStatus processTrade(){
+  public HttpStatus processTrade(@RequestBody final TradeDTO tradeDTO){
     return HttpStatus.OK;
   }
 }
