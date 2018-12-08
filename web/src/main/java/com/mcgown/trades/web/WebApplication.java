b4c92452194607ba5884e718ca719c49651374ed
Brian McGowan <bmcgowan@overstock.com>
2018-12-08 19:54:49 +0000

    adding web controller

--- web/src/main/java/com/mcgown/trades/web/WebApplication.java
+++ web/src/main/java/com/mcgown/trades/web/WebApplication.java
@@ -1,4 +1,14 @@
 package com.mcgown.trades.web;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@RequestMapping("api")
 public class WebApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(WebApplication.class, args);
+  }
+
 }
--- web/src/main/java/com/mcgown/trades/web/controller/ApiController.java
+++ web/src/main/java/com/mcgown/trades/web/controller/ApiController.java
@@ -1,4 +1,17 @@
 package com.mcgown.trades.web.controller;
 
+import lombok.extern.log4j.Log4j2;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log4j2
 public class ApiController {
+
+  @PostMapping("/trade")
+  public HttpStatus processTrade(){
+    return HttpStatus.OK;
+  }
 }
